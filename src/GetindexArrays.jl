7026c4028c885e76a914826c46738120ed29cee6
Tim Holy <tim.holy@gmail.com>
2021-10-02 03:07:22 -0500

    Store `extras` in struct
    This alternative implementation makes it easier to construct arrays
    where indexing depends on some additional data without needing to
    fear non-constant globals.  This comes at the cost of needing to
    provide an extra argument to the supplied `getindex` function.
    
    It also passes the index as a tuple. This makes it more likely to be
    automatically specialized than the `Vararg` would be.

--- src/GetindexArrays.jl
+++ src/GetindexArrays.jl
@@ -2,18 +2,19 @@ module GetindexArrays
 
 export GetindexArray
 
-struct GetindexArray{T,N,F,Axs<:NTuple{N,AbstractUnitRange{Int}}} <: AbstractArray{T,N}
+struct GetindexArray{T,N,F,Axs<:NTuple{N,AbstractUnitRange{Int}},Ex} <: AbstractArray{T,N}
     getindex::F
     axes::Axs
+    extras::Ex     # captured data needed to implement `getindex`
 end
-GetindexArray{T}(getindex::F, axes) where {T,F} =
-    GetindexArray{T, length(axes), F, typeof(axes)}(getindex, axes)
-GetindexArray(getindex::F, axes) where F =
-    GetindexArray{Base._return_type(getindex, NTuple{length(axes), Int})}(getindex, axes)
+GetindexArray{T}(getindex::F, axes, extras=nothing) where {T,F} =
+    GetindexArray{T, length(axes), F, typeof(axes), typeof(extras)}(getindex, axes, extras)
+GetindexArray(getindex::F, axes, extras=nothing) where F =
+    GetindexArray{Base._return_type(getindex, Tuple{typeof(extras), NTuple{length(axes), Int}})}(getindex, axes, extras)
 
 Base.@propagate_inbounds function Base.getindex(A::GetindexArray{T,N}, i::Vararg{Int,N}) where {T,N}
     @boundscheck checkbounds(A, i...)
-    convert(T, A.getindex(i...))::T
+    convert(T, A.getindex(A.extras, i))::T
 end
 Base.size(A::GetindexArray) = map(length, A.axes)
 Base.axes(A::GetindexArray) = A.axes
