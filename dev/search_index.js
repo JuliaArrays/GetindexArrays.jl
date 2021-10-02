624e1b7cde28ba8914cd56e6a88c48d6c3d5c1b9
Documenter.jl <documenter@juliadocs.github.io>
2021-10-02 08:29:01 +0000

    build based on 571050b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GetindexArrays","category":"page"},{"location":"#GetindexArrays","page":"Home","title":"GetindexArrays","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GetindexArrays.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GetindexArrays]","category":"page"}]
+}
